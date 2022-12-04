988cf451e50c10e378cc19a8fdd0d4c9b292e3b7
Carl J White III <carljwhite3@yahoo.com>
2022-12-03 23:11:35 -0600

    add example source

---
+++ example.cpp
@@ -0,0 +1,24 @@
+#include "RaspberryPiI2C.h"
+#include "Adafruit_BME280.h"
+#include "Adafruit_I2CDevice_Adapter.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    RaspberryPiI2C device(BME280_ADDRESS);
+    Adafruit_I2CDevice_Adapter adapter(device);
+
+    Adafruit_BME280 sensor(adapter);
+    
+    if (sensor.begin())
+    {
+        cout << "Temperature = " << sensor.readTemperature() << " C" << endl;
+        cout << "Pressure = " << sensor.readPressure()/100 <<" hPa" << endl;
+        cout << "Humidity = " << sensor.readHumidity() << " %" << endl;
+    }
+
+    return 0;
+}
